7db0cee605391e3b3c28eab2be183600ed1ebe13
Kemal Hatunoğlu <kemalhatunoglu@gmail.com>
2022-02-15 09:29:04 +0300

    Add moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandListComponent } from './features/brands/components/brand-list/brand-list.component';
+import { BrandAddComponent } from './features/brands/components/brand-add/brand-add.component';
+
+import { ListboxModule } from 'primeng/listbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from './layout/layout.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandListComponent,
+    BrandAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    LayoutModule,
+    ListboxModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/rentals/models/modelListModel.ts
@@ -0,0 +1,10 @@
+export interface ModelListModel {
+  id: number;
+  brandName: string;
+  colorName: string;
+  transmissonName: string;
+  modalName: string;
+  dailyPrice: number;
+  minScore: number;
+  imageUrl: string;
+}
